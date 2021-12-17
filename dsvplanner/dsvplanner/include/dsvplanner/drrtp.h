fc2b49b8a8467465213b4ee4f031f29fd9294598
dpc <450420922@qq.com>
2021-12-17 22:27:27 +0800

    annotation grid.h

--- dsvplanner/dsvplanner/include/dsvplanner/drrtp.h
+++ dsvplanner/dsvplanner/include/dsvplanner/drrtp.h
@@ -20,60 +20,85 @@ Hongbiao Zhu(hongbiaz@andrew.cmu.edu)
 #include "octomap_world/octomap_manager.h"
 
 namespace dsvplanner_ns {
-class drrtPlanner {
-public:
-  ros::NodeHandle nh_;
-  ros::NodeHandle nh_private_;
+    class drrtPlanner {
+    public:
+        ros::NodeHandle nh_;
+        ros::NodeHandle nh_private_;
 
-  ros::Subscriber odomSub_;
-  ros::Subscriber boundarySub_;
+        ros::Subscriber odomSub_;
+        ros::Subscriber boundarySub_;
 
-  ros::ServiceServer plannerService_;
-  ros::ServiceServer cleanFrontierService_;
+        ros::ServiceServer plannerService_;
+        ros::ServiceServer cleanFrontierService_;
 
-  Params params_;
-  volumetric_mapping::OctomapManager *manager_;
-  DualStateGraph *dual_state_graph_;
-  DualStateFrontier *dual_state_frontier_;
-  Drrt *drrt_;
-  OccupancyGrid *grid_;
+        Params params_;
+        volumetric_mapping::OctomapManager *manager_;
+        DualStateGraph *dual_state_graph_;
+        DualStateFrontier *dual_state_frontier_;
+        Drrt *drrt_;
+        OccupancyGrid *grid_;
 
-  bool init();
-  bool setParams();
-  // bool setPublisherPointer();
-  void odomCallback(const nav_msgs::Odometry &pose);
-  void boundaryCallback(const geometry_msgs::PolygonStamped &boundary);
-  bool plannerServiceCallback(dsvplanner::dsvplanner_srv::Request &req,
-                              dsvplanner::dsvplanner_srv::Response &res);
-  bool
-  cleanFrontierServiceCallback(dsvplanner::clean_frontier_srv::Request &req,
-                               dsvplanner::clean_frontier_srv::Response &res);
-  void cleanLastSelectedGlobalFrontier();
+        /**
+         * @brief 包含话题、服务初始化和参数设置
+        */
+        bool init();
 
-  drrtPlanner(const ros::NodeHandle &nh, const ros::NodeHandle &nh_private);
-  ~drrtPlanner();
+        /**
+         * @brief 参数设置
+        */
+        bool setParams();
 
-private:
-  std::string odomSubTopic;
-  std::string boundarySubTopic;
-  std::string newTreePathPubTopic;
-  std::string remainingTreePathPubTopic;
-  std::string boundaryPubTopic;
-  std::string globalSelectedFrontierPubTopic;
-  std::string localSelectedFrontierPubTopic;
-  std::string plantimePubTopic;
-  std::string nextGoalPubTopic;
-  std::string randomSampledPointsPubTopic;
-  std::string shutDownTopic;
-  std::string plannerServiceName;
-  std::string cleanFrontierServiceName;
+        // bool setPublisherPointer();
+        
+        /**
+         * @brief 订阅里程计
+        */
+        void odomCallback(const nav_msgs::Odometry &pose);
+        
+        /**
+         * @brief 订阅运动边界
+        */
+        void boundaryCallback(const geometry_msgs::PolygonStamped &boundary);
+        
+        /**
+         * @brief 获取规划状态的操作
+        */
+        bool plannerServiceCallback(dsvplanner::dsvplanner_srv::Request &req, dsvplanner::dsvplanner_srv::Response &res);
+        
+        /**
+         * @brief 清理Frontier的服务
+        */
+        bool cleanFrontierServiceCallback(dsvplanner::clean_frontier_srv::Request &req, dsvplanner::clean_frontier_srv::Response &res);
+        
+        /**
+         * @brief Clear old tree and the last global frontier.
+        */
+        void cleanLastSelectedGlobalFrontier();
 
-  std::chrono::steady_clock::time_point plan_start_;
-  std::chrono::steady_clock::time_point RRT_generate_over_;
-  std::chrono::steady_clock::time_point gain_computation_over_;
-  std::chrono::steady_clock::time_point plan_over_;
-  std::chrono::steady_clock::duration time_span;
-};
+        drrtPlanner(const ros::NodeHandle &nh, const ros::NodeHandle &nh_private);
+        ~drrtPlanner();
+
+        private:
+        std::string odomSubTopic;
+        std::string boundarySubTopic;
+        std::string newTreePathPubTopic;
+        std::string remainingTreePathPubTopic;
+        std::string boundaryPubTopic;
+        std::string globalSelectedFrontierPubTopic;
+        std::string localSelectedFrontierPubTopic;
+        std::string plantimePubTopic;
+        std::string nextGoalPubTopic;
+        std::string randomSampledPointsPubTopic;
+        std::string shutDownTopic;
+        std::string plannerServiceName;
+        std::string cleanFrontierServiceName;
+
+        std::chrono::steady_clock::time_point plan_start_;
+        std::chrono::steady_clock::time_point RRT_generate_over_;
+        std::chrono::steady_clock::time_point gain_computation_over_;
+        std::chrono::steady_clock::time_point plan_over_;
+        std::chrono::steady_clock::duration time_span;
+    };
 }
 
 #endif // DRRTP_H
--- dsvplanner/dsvplanner/include/dsvplanner/grid.h
+++ dsvplanner/dsvplanner/include/dsvplanner/grid.h
@@ -24,99 +24,142 @@ Hongbiao Zhu(hongbiaz@andrew.cmu.edu)
 
 using namespace Eigen;
 namespace dsvplanner_ns {
-typedef Vector3d StateVec;
-typedef Vector2i GridIndex;
-class OccupancyGrid {
-  typedef std::shared_ptr<OccupancyGrid> Ptr;
+    typedef Vector3d StateVec;
+    typedef Vector2i GridIndex;
+    class OccupancyGrid {
+        typedef std::shared_ptr<OccupancyGrid> Ptr;
 
-public:
-  OccupancyGrid(const ros::NodeHandle &nh, const ros::NodeHandle &nh_private);
-  ~OccupancyGrid();
+        public:
+            OccupancyGrid(const ros::NodeHandle &nh, const ros::NodeHandle &nh_private);
+            ~OccupancyGrid();
 
-  bool readParameters();
-  bool initialize();
+            /**
+             * @brief 设置地图参数
+            */
+            bool readParameters();
+            
+            /**
+             * @brief 地图初始化，包含设置参数、初始化ros话题、设置地图大小
+            */
+            bool initialize();
 
-public:
-  ros::NodeHandle nh_;
-  ros::NodeHandle nh_private_;
+            public:
+            ros::NodeHandle nh_;
+            ros::NodeHandle nh_private_;
 
-  // ROS subscribers
-  message_filters::Subscriber<nav_msgs::Odometry> odom_sub_;
-  message_filters::Subscriber<sensor_msgs::PointCloud2>
-      terrain_point_cloud_sub_;
-  typedef message_filters::sync_policies::ApproximateTime<
-      nav_msgs::Odometry, sensor_msgs::PointCloud2>
-      syncPolicy;
-  typedef message_filters::Synchronizer<syncPolicy> Sync;
-  boost::shared_ptr<Sync> sync_;
+            // ROS subscribers
+            message_filters::Subscriber<nav_msgs::Odometry> odom_sub_;//订阅里程计点云
+            message_filters::Subscriber<sensor_msgs::PointCloud2> terrain_point_cloud_sub_;//订阅地形点云
+            typedef message_filters::sync_policies::ApproximateTime<nav_msgs::Odometry, sensor_msgs::PointCloud2> syncPolicy;
+            typedef message_filters::Synchronizer<syncPolicy> Sync;
+            boost::shared_ptr<Sync> sync_;
 
-  // ROS publishers
-  ros::Publisher grid_cloud_pub_;
+            // ROS publishers
+            ros::Publisher grid_cloud_pub_;//发布栅格地图（点云式）
 
-  // String constants
-  std::string world_frame_id_;
-  std::string sub_odom_topic_;
-  std::string sub_terrain_point_cloud_topic_;
-  std::string pub_grid_points_topic_;
+            // String constants
+            std::string world_frame_id_;//世界frame
+            std::string sub_odom_topic_;//订阅odom的话题
+            std::string sub_terrain_point_cloud_topic_;//订阅地形点云的话题
+            std::string pub_grid_points_topic_;//发布地图点的话题
 
-  // Constants
-  double kMapWidth;
-  double kGridSize;
-  double kDownsampleSize;
-  double kObstacleHeightThre;
-  double kFlyingObstacleHeightThre;
-  double kCollisionCheckX;
-  double kCollisionCheckY;
+            // Constants
+            double kMapWidth;//地图的宽
+            double kGridSize;//地图的大小
+            double kDownsampleSize;//下采样的大小
+            double kObstacleHeightThre;//障碍物的高度阈值
+            double kFlyingObstacleHeightThre;//飞行障碍物的高度阈值
+            double kCollisionCheckX;//碰撞检测x值
+            double kCollisionCheckY;//碰撞检测y值
 
-  // Variables
-  enum gridStatus { unknown = 0, free = 1, occupied = 2, near_occupied = 3 };
-  std::vector<std::vector<int>> gridState_;
-  int map_width_grid_num_;
-  int map_half_width_grid_num_;
+            // Variables
+            enum gridStatus { unknown = 0, free = 1, occupied = 2, near_occupied = 3 };//栅格的状态
+            std::vector<std::vector<int>> gridState_;//栅格地图
+            int map_width_grid_num_;//栅格地图的宽（格数）
+            int map_half_width_grid_num_;//栅格地图的宽的一半（格数）
 
-  ros::Time terrain_time_;
-  pcl::PointCloud<pcl::PointXYZI>::Ptr terrain_cloud_ =
-      pcl::PointCloud<pcl::PointXYZI>::Ptr(
-          new pcl::PointCloud<pcl::PointXYZI>());
-  pcl::PointCloud<pcl::PointXYZI>::Ptr terrain_cloud_ds =
-      pcl::PointCloud<pcl::PointXYZI>::Ptr(
-          new pcl::PointCloud<pcl::PointXYZI>());
-  pcl::PointCloud<pcl::PointXYZI>::Ptr terrain_cloud_traversable_ =
-      pcl::PointCloud<pcl::PointXYZI>::Ptr(
-          new pcl::PointCloud<pcl::PointXYZI>());
-  pcl::PointCloud<pcl::PointXYZI>::Ptr terrain_cloud_obstacle_ =
-      pcl::PointCloud<pcl::PointXYZI>::Ptr(
-          new pcl::PointCloud<pcl::PointXYZI>());
-  pcl::PointCloud<pcl::PointXYZI>::Ptr grid_cloud_ =
-      pcl::PointCloud<pcl::PointXYZI>::Ptr(
-          new pcl::PointCloud<pcl::PointXYZI>());
+            ros::Time terrain_time_;
+            pcl::PointCloud<pcl::PointXYZI>::Ptr terrain_cloud_ = pcl::PointCloud<pcl::PointXYZI>::Ptr(new pcl::PointCloud<pcl::PointXYZI>());//地形点云
+            pcl::PointCloud<pcl::PointXYZI>::Ptr terrain_cloud_ds = pcl::PointCloud<pcl::PointXYZI>::Ptr(new pcl::PointCloud<pcl::PointXYZI>());//地形点云下采样
+            pcl::PointCloud<pcl::PointXYZI>::Ptr terrain_cloud_traversable_ = pcl::PointCloud<pcl::PointXYZI>::Ptr(new pcl::PointCloud<pcl::PointXYZI>());//地形点云中可通过的点云
+            pcl::PointCloud<pcl::PointXYZI>::Ptr terrain_cloud_obstacle_ = pcl::PointCloud<pcl::PointXYZI>::Ptr(new pcl::PointCloud<pcl::PointXYZI>());//地形点云中视为障碍物的点云
+            pcl::PointCloud<pcl::PointXYZI>::Ptr grid_cloud_ = pcl::PointCloud<pcl::PointXYZI>::Ptr(new pcl::PointCloud<pcl::PointXYZI>());
 
-  StateVec robot_position_;
+            StateVec robot_position_;//机器人位置
 
-  // Functions
-  geometry_msgs::Point getPoint(GridIndex p);
-  GridIndex getIndex(StateVec point);
-  void clearGrid();
-  void updateGrid();
-  void publishGridMap();
-  bool collisionCheckByTerrainWithVector(StateVec origin_point,
-                                         StateVec goal_point);
-  bool collisionCheckByTerrain(geometry_msgs::Point origin,
-                               geometry_msgs::Point goal);
-  bool InRange(const GridIndex sub, const GridIndex max_sub,
-               const GridIndex min_sub);
-  bool updateFreeGridWithSurroundingGrids(int indx, int indy);
-  int signum(int x);
-  double intbound(double s, double ds);
-  double mod(double value, double modulus);
-  std::vector<GridIndex> rayCast(GridIndex origin, GridIndex goal,
-                                 GridIndex max_grid, GridIndex min_grid);
+            // Functions
 
-  // Callback Functions
-  void terrainCloudAndOdomCallback(
-      const nav_msgs::Odometry::ConstPtr &odom_msg,
-      const sensor_msgs::PointCloud2::ConstPtr &terrain_msg);
-};
+            /**
+             * @brief 以p对应栅格的中心生成点
+            */
+            geometry_msgs::Point getPoint(GridIndex p);
+
+            /**
+             * @brief 计算点在栅格地图中的坐标
+            */
+            GridIndex getIndex(StateVec point);
+            
+            /**
+             * @brief 将栅格地图的所有点，置为未知
+            */
+            void clearGrid();
+
+            /**
+             * @brief 根据terrain_cloud_obstacle_和terrain_cloud_traversable_更新栅格的状态
+            */
+            void updateGrid();
+
+            /**
+             * @brief 将每个栅格中的状态填入到点的属性中，并中心位置为点坐标发布出去
+            */
+            void publishGridMap();
+
+            /**
+             * @brief 检测起点和终点之间是否有障碍物（射线法）
+            */
+            bool collisionCheckByTerrainWithVector(StateVec origin_point, StateVec goal_point);
+
+            /**
+             * @brief 检测起点和终点之间是否有障碍物
+            */
+            bool collisionCheckByTerrain(geometry_msgs::Point origin, geometry_msgs::Point goal);
+            
+            /**
+             * @brief 点是否在指定的范围内
+            */
+            bool InRange(const GridIndex sub, const GridIndex max_sub, const GridIndex min_sub);
+            
+            /**
+             * @brief 根据对应点对应的附近是否有被占用的点
+            */
+            bool updateFreeGridWithSurroundingGrids(int indx, int indy);
+            
+            /**
+             * @brief signmod函数 小于0为-1，大于0为1
+            */
+            int signum(int x);
+
+            /**
+             * @brief 
+            */
+            double intbound(double s, double ds);
+
+            /**
+             * @brief 取余，采用fmod进行了两次取余，//FIXME:不知道两次的意义
+            */
+            double mod(double value, double modulus);
+
+            /**
+             * @brief 射线法计算起点和终点之间所有的点的坐标
+            */
+            std::vector<GridIndex> rayCast(GridIndex origin, GridIndex goal, GridIndex max_grid, GridIndex min_grid);
+
+            // Callback Functions
+            /**
+             * @brief 将获得的地形点云分类为可通过和障碍物
+            */
+            void terrainCloudAndOdomCallback(const nav_msgs::Odometry::ConstPtr &odom_msg, const sensor_msgs::PointCloud2::ConstPtr &terrain_msg);
+    };
 }
 
 #endif // GRID_H
